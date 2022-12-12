f1251ed50f054d9d25ebc9069d9e7b629c330937
if12is <aa1980461@gmail.com>
2022-12-13 01:55:47 +0200

    first commit

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Resources\UserResource\Pages\EditUser;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Filament\Resources\UserResource\RelationManagers\RolesRelationManager;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Page;
+use Filament\Resources\Form;
+use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    protected static ?string $navigationGroup = 'Admin Management';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    // protected static bool $shouldRegisterNavigation = false;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('is_admin')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('password')
+                    ->password()
+                    ->maxLength(255)
+                    // ->same('passwordConfirmation')
+                    ->minLength(8)
+                    // when Update the user information To skip error messages password , do these
+                    ->dehydrateStateUsing(
+                        static fn (null|string $state): null|string =>
+                        filled($state) ? Hash::make($state) : null,
+                    )->required(
+                        static fn (Page $livewire): bool =>
+                        $livewire instanceof CreateUser,
+                    )->dehydrated(
+                        static fn (null|string $state): bool =>
+                        filled($state),
+                    )->label(
+                        static fn (Page $livewire): string => ($livewire instanceof EditUser) ? 'New Password' : 'Password'
+                    ),
+                // TextInput::make("passwordConfirmation")
+                //     ->password()
+                //     ->label('Password Confirmation')
+                //     ->required(fn (Page $livewire): bool => $livewire instanceof CreateRecord)
+                //     ->minLength(8)
+                //     ->dehydrated(false),
+                // anther solution
+                // TextInput::make('password')
+                //     ->password()
+                //     ->required(fn (Page $livewire): bool => $livewire instanceof CreateRecord)
+                //     ->minLength(8)
+                //     ->same('passwordConfirmation')
+                //     ->dehydrated(fn ($state) => filled($state))
+                //     ->dehydrateStateUsing(fn ($state) => Hash::make($state)),
+                CheckboxList::make('roles')
+                    ->relationship('roles', 'name')
+                    ->columns(1)
+                    ->helperText('Only Choose One role')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('is_admin')
+                    ->sortable()
+                    ->searchable()
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('email'),
+                TextColumn::make('roles.name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->sortable()
+                    ->searchable()
+                    ->dateTime('d-M-Y'),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->sortable()
+                    ->searchable()
+                    ->dateTime('d-M-Y'),
+
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            RolesRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
